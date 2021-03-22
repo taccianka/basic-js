2745ef8cf02c698a9bae232e445d543c4ac71351
taccianka <tacciankastudy@gmail.com>
2021-03-22 13:32:38 +0300

    fix: fix bugs in task "Carbon dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,17 +7,17 @@ module.exports = function dateSample(sampleActivity) {
   // throw new CustomError('Not implemented');
   if (
     typeof sampleActivity != "string" ||
-    /["0"-"9"]/.test(sampleActivity) == false
+    /^[0-9]*[.,]?[0-9]+$/.test(sampleActivity) == false ||
+    +sampleActivity <= 0 ||
+    +sampleActivity > MODERN_ACTIVITY
   )
     return false;
 
   let t;
-  sampleActivity = +sampleActivity;
-  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  sampleActivity = Number(sampleActivity);
+  const k = 0.693 / HALF_LIFE_PERIOD;
   let a0a = MODERN_ACTIVITY / sampleActivity;
-
-  t = Math.log1p(a0a) / k;
-
+  t = Math.log(a0a) / k;
   return Math.ceil(t);
   // remove line with error and write your code here
 };
