d153c9b47837db038ca268f308139375da9580a6
taccianka <tacciankastudy@gmail.com>
2021-03-27 00:07:00 +0300

    fix: fix bugs with "transform-array" task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,18 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  /* throw new CustomError('Not implemented');*/
-  // remove line with error and write your code here
-
   if (!Array.isArray(arr)) throw Error("THROWN");
 
   let array = [];
   let flag = 0;
 
-  // console.log(arr, "\n\n");
   for (let i = 0; i < arr.length; i++) {
     if (typeof arr[i] != "string") {
       array.push(arr[i]);
+      flag = 0;
     } else if (typeof arr[i] == "string") {
       switch (arr[i]) {
         case "--discard-next":
@@ -21,28 +18,29 @@ module.exports = function transform(arr) {
           break;
         case "--discard-prev":
           if (!flag) array.pop(arr[i]);
-          else flag = 0;
+          flag = 0;
           break;
         case "--double-next":
-          if (arr[i + 1]) array.push(arr[i + 1]);
+          if (!(arr[i + 1] === undefined)) {
+            array.push(arr[i + 1]);
+          }
+          flag = 0;
           break;
         case "--double-prev":
-          if (arr[i - 1]) {
-            if (!flag) array.push(arr[i - 1]);
-            else flag = 0;
+          if (!(arr[i - 1] === undefined)) {
+            if (!flag) {
+              array.push(arr[i - 1]);
+            }
           }
+          flag = 0;
           break;
         default:
           array.push(arr[i]);
+          flag = 0;
           break;
       }
     }
   }
 
-  /*  array.forEach((element) => {
-    if (Array.isArray(element)) transform(element);
-  });
-*/
-  // console.log("array =", array);
   return array;
 };
