91dec23e6c15ce64cc19ec9130f2978c1fcd8062
taccianka <tacciankastudy@gmail.com>
2021-03-27 03:23:17 +0300

    feat: implement chain-maker-task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position > this.getLength()) {
+      this.chain = [];
+      throw new Error("error");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = this.chain.join("~~");
+    this.chain = [];
+    return result;
+  },
 };
 
 module.exports = chainMaker;
