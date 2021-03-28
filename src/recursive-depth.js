a2cff721f8b993a41f8b8b4a67cdd54e6b2118b6
taccianka <tacciankastudy@gmail.com>
2021-03-28 03:13:52 +0300

    feat: implement "recursive-depth" task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,10 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError("Not implemented");
-    // remove line with error and write your code here
+  constructor() {
+    this.maxDepth = 0;
+    this.count = 0;
   }
+
+  maxDepth;
+  count;
+
+  calculateDepth(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.count++;
+        this.calculateDepth(arr[i]);
+      }
+    }
+    if (this.count > this.maxDepth) {
+      this.maxDepth = this.count;
+      this.count = 0;
+    } else this.count = 0;
+    return this.maxDepth + 1;
+  }
+
+  /* function depth(array, rec) {
+      if (!Array.isArray(array)) throw new Exception('not an array');
+
+      var res = rec;
+      for(var i = 0; i < array.length; ++i) {
+          if (Array.isArray(array[i])) {
+          var subDepth = depth(array[i], rec + 1);
+          if (subDepth > res) {
+              res = subDepth;
+          }
+        }
+      }
+      return res;
+      }
+*/
+
+  /* if (Array.isArray(arr)){
+      count++;
+      if (arr.flat() == arr)
+      return;
+    }
+    for (let i = 0; i<arr.length; i++){
+      if (Array.isArray[arr[i]) {
+        this.count++;
+        this.calculateDepth(arr[i]);
+      } else return;
+    }*/
+
+  /*function flatDeep(arr, d = 1) {
+      return d > 0
+        ? arr.reduce(
+            (acc, val) =>
+              acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val),
+            []
+          )
+        : arr.slice();
+    }
+
+    console.log(flatDeep(arr, Infinity));*/
+
+  //return;
 };
 
 /*module.exports = class DepthCalculator {
